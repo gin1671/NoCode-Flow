4eee1e30089c0c7dd3131585be1becf8dd366c89
nyantech <nyantech14@gmail.com>
2020-09-13 18:59:12 +0900

    feat(top): test layout top

--- src/app/top/top.module.ts
+++ src/app/top/top.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { TopRoutingModule } from './top-routing.module';
 import { TopComponent } from './top.component';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [TopComponent],
-  imports: [
-    CommonModule,
-    TopRoutingModule
-  ]
+  imports: [CommonModule, TopRoutingModule, MatButtonModule],
 })
-export class TopModule { }
+export class TopModule {}
